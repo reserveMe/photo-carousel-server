a27e92aef65fbfb0de47e2f16aa1c293f931212e
Linden Chiu <lindentree@Lindens-MacBook-Pro.local>
2019-01-17 14:01:28 -0800

    first get route

--- database/index.postgres_dev.js
+++ database/index.postgres_dev.js
@@ -1,1 +1,21 @@
-const { Pool, Client } = require('pg')
+const { Pool, Client } = require('pg');
+const client = new Client();
+const config = require('./config.js');
+
+const pool = new Pool(config);
+
+//await client.connect();
+
+
+//get all route 
+const getRestaurantInfo = (restaurantID, callback) => {
+  pool.query(`SELECT * FROM restaurants WHERE ID = ${restaurantID}`)
+    .then(results => callback(null, results.rows))
+    .catch(err => callback(err));
+}
+
+//update restaurant photos route 
+
+//add restaurant(s) route
+
+//remove restaurant(s) route
